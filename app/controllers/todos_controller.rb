7a972c2f8cba566d509ad3b4fb46118234b3875b
ValeriaMejiaCortes <valemejiacortez@gmail.com>
2020-11-25 09:23:22 -0500

    WIP create todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,4 +2,23 @@ class TodosController < ApplicationController
   def index
     @todos = Todo.all
   end
+
+  # render a js file
+  def new
+    @todo = Todo.new
+
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def create
+    @todo = Todo.new(params.require(:todo).permit(:title, :text))
+
+    if @todo.save
+      redirect_to @todo
+    else
+      render 'new'
+    end
+  end
 end
